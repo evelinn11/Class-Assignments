51f1cc76a1216eb158b2ec2b5e4e65af064ff1e4
evelinn11 <139670444+evelinn11@users.noreply.github.com>
2024-04-19 23:20:52 +0700

    Add files via upload

---
+++ W03 - Class Assignment - Personal Data/W03 - Class Assignment - Personal Data/Personal Data.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace W03___Class_Assignment___Personal_Data
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_Name = new System.Windows.Forms.Label();
+            this.tB_Name = new System.Windows.Forms.TextBox();
+            this.lb_Age = new System.Windows.Forms.Label();
+            this.tB_Age = new System.Windows.Forms.TextBox();
+            this.lb_Gender = new System.Windows.Forms.Label();
+            this.panel_Gender = new System.Windows.Forms.Panel();
+            this.rBtn_Female = new System.Windows.Forms.RadioButton();
+            this.rBtn_Male = new System.Windows.Forms.RadioButton();
+            this.lb_FavColor = new System.Windows.Forms.Label();
+            this.panel_FavColor = new System.Windows.Forms.Panel();
+            this.rBtn_Yellow = new System.Windows.Forms.RadioButton();
+            this.rBtn_Blue = new System.Windows.Forms.RadioButton();
+            this.rBtn_Green = new System.Windows.Forms.RadioButton();
+            this.lb_Hobbies = new System.Windows.Forms.Label();
+            this.cB_Reading = new System.Windows.Forms.CheckBox();
+            this.cB_Watching = new System.Windows.Forms.CheckBox();
+            this.cB_Hobbies = new System.Windows.Forms.CheckBox();
+            this.cB_Playing = new System.Windows.Forms.CheckBox();
+            this.tB_Hobbies = new System.Windows.Forms.TextBox();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.panel_Gender.SuspendLayout();
+            this.panel_FavColor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lb_Name
+            // 
+            this.lb_Name.AutoSize = true;
+            this.lb_Name.Location = new System.Drawing.Point(19, 15);
+            this.lb_Name.Name = "lb_Name";
+            this.lb_Name.Size = new System.Drawing.Size(35, 13);
+            this.lb_Name.TabIndex = 0;
+            this.lb_Name.Text = "Name";
+            // 
+            // tB_Name
+            // 
+            this.tB_Name.Location = new System.Drawing.Point(73, 12);
+            this.tB_Name.Name = "tB_Name";
+            this.tB_Name.Size = new System.Drawing.Size(148, 20);
+            this.tB_Name.TabIndex = 1;
+            // 
+            // lb_Age
+            // 
+            this.lb_Age.AutoSize = true;
+            this.lb_Age.Location = new System.Drawing.Point(19, 46);
+            this.lb_Age.Name = "lb_Age";
+            this.lb_Age.Size = new System.Drawing.Size(26, 13);
+            this.lb_Age.TabIndex = 2;
+            this.lb_Age.Text = "Age";
+            // 
+            // tB_Age
+            // 
+            this.tB_Age.Location = new System.Drawing.Point(73, 43);
+            this.tB_Age.Name = "tB_Age";
+            this.tB_Age.Size = new System.Drawing.Size(148, 20);
+            this.tB_Age.TabIndex = 3;
+            // 
+            // lb_Gender
+            // 
+            this.lb_Gender.AutoSize = true;
+            this.lb_Gender.Location = new System.Drawing.Point(19, 78);
+            this.lb_Gender.Name = "lb_Gender";
+            this.lb_Gender.Size = new System.Drawing.Size(42, 13);
+            this.lb_Gender.TabIndex = 4;
+            this.lb_Gender.Text = "Gender";
+            // 
+            // panel_Gender
+            // 
+            this.panel_Gender.Controls.Add(this.rBtn_Female);
+            this.panel_Gender.Controls.Add(this.rBtn_Male);
+            this.panel_Gender.Location = new System.Drawing.Point(22, 94);
+            this.panel_Gender.Name = "panel_Gender";
+            this.panel_Gender.Size = new System.Drawing.Size(104, 62);
+            this.panel_Gender.TabIndex = 6;
+            // 
+            // rBtn_Female
+            // 
+            this.rBtn_Female.AutoSize = true;
+            this.rBtn_Female.Location = new System.Drawing.Point(3, 26);
+            this.rBtn_Female.Name = "rBtn_Female";
+            this.rBtn_Female.Size = new System.Drawing.Size(59, 17);
+            this.rBtn_Female.TabIndex = 1;
+            this.rBtn_Female.TabStop = true;
+            this.rBtn_Female.Text = "Female";
+            this.rBtn_Female.UseVisualStyleBackColor = true;
+            // 
+            // rBtn_Male
+            // 
+            this.rBtn_Male.AutoSize = true;
+            this.rBtn_Male.Location = new System.Drawing.Point(3, 3);
+            this.rBtn_Male.Name = "rBtn_Male";
+            this.rBtn_Male.Size = new System.Drawing.Size(48, 17);
+            this.rBtn_Male.TabIndex = 0;
+            this.rBtn_Male.TabStop = true;
+            this.rBtn_Male.Text = "Male";
+            this.rBtn_Male.UseVisualStyleBackColor = true;
+            // 
+            // lb_FavColor
+            // 
+            this.lb_FavColor.AutoSize = true;
+            this.lb_FavColor.Location = new System.Drawing.Point(19, 159);
+            this.lb_FavColor.Name = "lb_FavColor";
+            this.lb_FavColor.Size = new System.Drawing.Size(72, 13);
+            this.lb_FavColor.TabIndex = 7;
+            this.lb_FavColor.Text = "Favorite Color";
+            // 
+            // panel_FavColor
+            // 
+            this.panel_FavColor.Controls.Add(this.rBtn_Yellow);
+            this.panel_FavColor.Controls.Add(this.rBtn_Blue);
+            this.panel_FavColor.Controls.Add(this.rBtn_Green);
+            this.panel_FavColor.Location = new System.Drawing.Point(22, 175);
+            this.panel_FavColor.Name = "panel_FavColor";
+            this.panel_FavColor.Size = new System.Drawing.Size(104, 75);
+            this.panel_FavColor.TabIndex = 7;
+            // 
+            // rBtn_Yellow
+            // 
+            this.rBtn_Yellow.AutoSize = true;
+            this.rBtn_Yellow.Location = new System.Drawing.Point(3, 49);
+            this.rBtn_Yellow.Name = "rBtn_Yellow";
+            this.rBtn_Yellow.Size = new System.Drawing.Size(56, 17);
+            this.rBtn_Yellow.TabIndex = 2;
+            this.rBtn_Yellow.TabStop = true;
+            this.rBtn_Yellow.Text = "Yellow";
+            this.rBtn_Yellow.UseVisualStyleBackColor = true;
+            // 
+            // rBtn_Blue
+            // 
+            this.rBtn_Blue.AutoSize = true;
+            this.rBtn_Blue.Location = new System.Drawing.Point(3, 26);
+            this.rBtn_Blue.Name = "rBtn_Blue";
+            this.rBtn_Blue.Size = new System.Drawing.Size(46, 17);
+            this.rBtn_Blue.TabIndex = 1;
+            this.rBtn_Blue.TabStop = true;
+            this.rBtn_Blue.Text = "Blue";
+            this.rBtn_Blue.UseVisualStyleBackColor = true;
+            // 
+            // rBtn_Green
+            // 
+            this.rBtn_Green.AutoSize = true;
+            this.rBtn_Green.Location = new System.Drawing.Point(3, 3);
+            this.rBtn_Green.Name = "rBtn_Green";
+            this.rBtn_Green.Size = new System.Drawing.Size(54, 17);
+            this.rBtn_Green.TabIndex = 0;
+            this.rBtn_Green.TabStop = true;
+            this.rBtn_Green.Text = "Green";
+            this.rBtn_Green.UseVisualStyleBackColor = true;
+            // 
+            // lb_Hobbies
+            // 
+            this.lb_Hobbies.AutoSize = true;
+            this.lb_Hobbies.Location = new System.Drawing.Point(19, 263);
+            this.lb_Hobbies.Name = "lb_Hobbies";
+            this.lb_Hobbies.Size = new System.Drawing.Size(46, 13);
+            this.lb_Hobbies.TabIndex = 8;
+            this.lb_Hobbies.Text = "Hobbies";
+            // 
+            // cB_Reading
+            // 
+            this.cB_Reading.AutoSize = true;
+            this.cB_Reading.Location = new System.Drawing.Point(22, 289);
+            this.cB_Reading.Name = "cB_Reading";
+            this.cB_Reading.Size = new System.Drawing.Size(66, 17);
+            this.cB_Reading.TabIndex = 9;
+            this.cB_Reading.Text = "Reading";
+            this.cB_Reading.UseVisualStyleBackColor = true;
+            // 
+            // cB_Watching
+            // 
+            this.cB_Watching.AutoSize = true;
+            this.cB_Watching.Location = new System.Drawing.Point(22, 312);
+            this.cB_Watching.Name = "cB_Watching";
+            this.cB_Watching.Size = new System.Drawing.Size(89, 17);
+            this.cB_Watching.TabIndex = 10;
+            this.cB_Watching.Text = "Watching TV";
+            this.cB_Watching.UseVisualStyleBackColor = true;
+            // 
+            // cB_Hobbies
+            // 
+            this.cB_Hobbies.AutoSize = true;
+            this.cB_Hobbies.Location = new System.Drawing.Point(138, 312);
+            this.cB_Hobbies.Name = "cB_Hobbies";
+            this.cB_Hobbies.Size = new System.Drawing.Size(15, 14);
+            this.cB_Hobbies.TabIndex = 12;
+            this.cB_Hobbies.UseVisualStyleBackColor = true;
+            // 
+            // cB_Playing
+            // 
+            this.cB_Playing.AutoSize = true;
+            this.cB_Playing.Location = new System.Drawing.Point(138, 289);
+            this.cB_Playing.Name = "cB_Playing";
+            this.cB_Playing.Size = new System.Drawing.Size(93, 17);
+            this.cB_Playing.TabIndex = 11;
+            this.cB_Playing.Text = "Playing Sports";
+            this.cB_Playing.UseVisualStyleBackColor = true;
+            // 
+            // tB_Hobbies
+            // 
+            this.tB_Hobbies.Location = new System.Drawing.Point(159, 309);
+            this.tB_Hobbies.Name = "tB_Hobbies";
+            this.tB_Hobbies.Size = new System.Drawing.Size(80, 20);
+            this.tB_Hobbies.TabIndex = 13;
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.Location = new System.Drawing.Point(26, 348);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(100, 29);
+            this.btn_Submit.TabIndex = 14;
+            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.UseVisualStyleBackColor = true;
+            this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(138, 348);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(101, 29);
+            this.btn_Clear.TabIndex = 15;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(292, 391);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_Submit);
+            this.Controls.Add(this.tB_Hobbies);
+            this.Controls.Add(this.cB_Hobbies);
+            this.Controls.Add(this.cB_Playing);
+            this.Controls.Add(this.cB_Watching);
+            this.Controls.Add(this.cB_Reading);
+            this.Controls.Add(this.lb_Hobbies);
+            this.Controls.Add(this.panel_FavColor);
+            this.Controls.Add(this.lb_FavColor);
+            this.Controls.Add(this.panel_Gender);
+            this.Controls.Add(this.lb_Gender);
+            this.Controls.Add(this.tB_Age);
+            this.Controls.Add(this.lb_Age);
+            this.Controls.Add(this.tB_Name);
+            this.Controls.Add(this.lb_Name);
+            this.Name = "Form1";
+            this.Text = "Personal Data";
+            this.panel_Gender.ResumeLayout(false);
+            this.panel_Gender.PerformLayout();
+            this.panel_FavColor.ResumeLayout(false);
+            this.panel_FavColor.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_Name;
+        private System.Windows.Forms.TextBox tB_Name;
+        private System.Windows.Forms.Label lb_Age;
+        private System.Windows.Forms.TextBox tB_Age;
+        private System.Windows.Forms.Label lb_Gender;
+        private System.Windows.Forms.Panel panel_Gender;
+        private System.Windows.Forms.RadioButton rBtn_Female;
+        private System.Windows.Forms.RadioButton rBtn_Male;
+        private System.Windows.Forms.Label lb_FavColor;
+        private System.Windows.Forms.Panel panel_FavColor;
+        private System.Windows.Forms.RadioButton rBtn_Blue;
+        private System.Windows.Forms.RadioButton rBtn_Green;
+        private System.Windows.Forms.RadioButton rBtn_Yellow;
+        private System.Windows.Forms.Label lb_Hobbies;
+        private System.Windows.Forms.CheckBox cB_Reading;
+        private System.Windows.Forms.CheckBox cB_Watching;
+        private System.Windows.Forms.CheckBox cB_Hobbies;
+        private System.Windows.Forms.CheckBox cB_Playing;
+        private System.Windows.Forms.TextBox tB_Hobbies;
+        private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.Button btn_Clear;
+    }
+}
+
---
+++ W03 - Class Assignment - Personal Data/W03 - Class Assignment - Personal Data/Personal Data.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W03___Class_Assignment___Personal_Data
+{
+    public partial class Form1 : Form
+    {
+        //Evelin Alim Natadjaja
+        //NIM = 0706022310021
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+            bool errorDetected = false;
+            List<string> listAngka = new List<string>() { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            string listHuruf = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxyz ";
+
+            foreach (string angka in listAngka)
+            {
+                if (tB_Name.Text.Contains(angka))
+                {
+                    errorDetected = true;
+                }
+
+                if (tB_Hobbies.Text.Contains(angka))
+                {
+                    errorDetected = true;
+                }
+            }
+
+            foreach(char huruf in listHuruf)
+            {
+                if (tB_Age.Text.Contains(huruf))
+                {
+                    errorDetected = true;
+                }
+            }
+
+            if (errorDetected == true)
+            {
+                MessageBox.Show("Error : Text Box has incorrect input");
+            }
+            else if (errorDetected == false)
+            {
+                string genderChosen;
+                if (rBtn_Male.Checked == true)
+                {
+                    genderChosen = rBtn_Male.Text;
+                }
+                else
+                {
+                    genderChosen = rBtn_Female.Text;
+                }
+
+                List<string> hobbiesChosen = new List<string>();
+                if (cB_Reading.Checked == true)
+                {
+                    hobbiesChosen.Add(cB_Reading.Text);
+                }
+                if (cB_Watching.Checked == true)
+                {
+                    hobbiesChosen.Add(cB_Watching.Text);
+                }
+                if (cB_Playing.Checked == true)
+                {
+                    hobbiesChosen.Add(cB_Playing.Text);
+                }
+                if (cB_Hobbies.Checked == true)
+                {
+                    hobbiesChosen.Add(tB_Hobbies.Text);
+                }
+
+                string hobbiesShown = "";
+                for (int i = 0; i < hobbiesChosen.Count - 1; i++)
+                {
+                    hobbiesShown += hobbiesChosen[i] + ", ";
+                }
+
+                hobbiesShown += hobbiesChosen[hobbiesChosen.Count - 1];
+
+                if (rBtn_Green.Checked == true)
+                {
+                    this.BackColor = Color.Green;
+                }
+                else if (rBtn_Blue.Checked == true)
+                {
+                    this.BackColor = Color.Blue;
+                }
+                else if (rBtn_Yellow.Checked == true)
+                {
+                    this.BackColor = Color.Yellow;
+                }
+
+                MessageBox.Show($"Name: {tB_Name.Text} \nAge: {tB_Age.Text} \nGender: {genderChosen}" +
+                    $" \nHobbies: {hobbiesShown}");
+
+            }
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            tB_Name.Text = string.Empty;
+            tB_Age.Text = string.Empty;
+            tB_Hobbies.Text = string.Empty;
+            rBtn_Blue.Checked = false;
+            rBtn_Yellow.Checked = false;
+            rBtn_Green.Checked = false;
+            rBtn_Male.Checked = false;
+            rBtn_Female.Checked = false;
+            cB_Reading.Checked = false;
+            cB_Watching.Checked = false;
+            cB_Playing.Checked = false;
+            cB_Hobbies.Checked = false;
+            this.BackColor = SystemColors.Control;
+        }
+    }
+}
