259bddf3ebc75ccf6918afb0e97951e9a3c3177c
evelinn11 <139670444+evelinn11@users.noreply.github.com>
2024-04-19 23:53:54 +0700

    Add files via upload

---
+++ W06 - Class Assignment - Puzzle/W06 - Class Assignment - Puzzle/Puzzle.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace W06___Class_Assignment___Puzzle
+{
+    partial class Puzzle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_InputView = new System.Windows.Forms.Panel();
+            this.btn_Play = new System.Windows.Forms.Button();
+            this.tB_Input = new System.Windows.Forms.TextBox();
+            this.lb_Input = new System.Windows.Forms.Label();
+            this.lb_Welcome = new System.Windows.Forms.Label();
+            this.lb_Red = new System.Windows.Forms.Label();
+            this.lb_Blue = new System.Windows.Forms.Label();
+            this.lb_CountRed = new System.Windows.Forms.Label();
+            this.lb_CountBlue = new System.Windows.Forms.Label();
+            this.panel_Score = new System.Windows.Forms.Panel();
+            this.panel_InputView.SuspendLayout();
+            this.panel_Score.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_InputView
+            // 
+            this.panel_InputView.Controls.Add(this.btn_Play);
+            this.panel_InputView.Controls.Add(this.tB_Input);
+            this.panel_InputView.Controls.Add(this.lb_Input);
+            this.panel_InputView.Controls.Add(this.lb_Welcome);
+            this.panel_InputView.Location = new System.Drawing.Point(4, 12);
+            this.panel_InputView.Name = "panel_InputView";
+            this.panel_InputView.Size = new System.Drawing.Size(267, 193);
+            this.panel_InputView.TabIndex = 0;
+            this.panel_InputView.VisibleChanged += new System.EventHandler(this.panel_InputView_VisibleChanged);
+            // 
+            // btn_Play
+            // 
+            this.btn_Play.Location = new System.Drawing.Point(103, 143);
+            this.btn_Play.Name = "btn_Play";
+            this.btn_Play.Size = new System.Drawing.Size(49, 23);
+            this.btn_Play.TabIndex = 3;
+            this.btn_Play.Text = "PLAY";
+            this.btn_Play.UseVisualStyleBackColor = true;
+            this.btn_Play.Click += new System.EventHandler(this.btn_Play_Click);
+            // 
+            // tB_Input
+            // 
+            this.tB_Input.Location = new System.Drawing.Point(86, 107);
+            this.tB_Input.Name = "tB_Input";
+            this.tB_Input.Size = new System.Drawing.Size(87, 20);
+            this.tB_Input.TabIndex = 2;
+            this.tB_Input.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tB_Input_KeyPress);
+            // 
+            // lb_Input
+            // 
+            this.lb_Input.AutoSize = true;
+            this.lb_Input.Location = new System.Drawing.Point(72, 81);
+            this.lb_Input.Name = "lb_Input";
+            this.lb_Input.Size = new System.Drawing.Size(113, 13);
+            this.lb_Input.TabIndex = 1;
+            this.lb_Input.Text = "Input Your Puzzle Size";
+            // 
+            // lb_Welcome
+            // 
+            this.lb_Welcome.AutoSize = true;
+            this.lb_Welcome.Location = new System.Drawing.Point(100, 38);
+            this.lb_Welcome.Name = "lb_Welcome";
+            this.lb_Welcome.Size = new System.Drawing.Size(52, 13);
+            this.lb_Welcome.TabIndex = 0;
+            this.lb_Welcome.Text = "Welcome";
+            // 
+            // lb_Red
+            // 
+            this.lb_Red.AutoSize = true;
+            this.lb_Red.Location = new System.Drawing.Point(19, 18);
+            this.lb_Red.Name = "lb_Red";
+            this.lb_Red.Size = new System.Drawing.Size(30, 13);
+            this.lb_Red.TabIndex = 1;
+            this.lb_Red.Text = "RED";
+            // 
+            // lb_Blue
+            // 
+            this.lb_Blue.AutoSize = true;
+            this.lb_Blue.Location = new System.Drawing.Point(95, 18);
+            this.lb_Blue.Name = "lb_Blue";
+            this.lb_Blue.Size = new System.Drawing.Size(35, 13);
+            this.lb_Blue.TabIndex = 2;
+            this.lb_Blue.Text = "BLUE";
+            // 
+            // lb_CountRed
+            // 
+            this.lb_CountRed.AutoSize = true;
+            this.lb_CountRed.Location = new System.Drawing.Point(19, 50);
+            this.lb_CountRed.Name = "lb_CountRed";
+            this.lb_CountRed.Size = new System.Drawing.Size(13, 13);
+            this.lb_CountRed.TabIndex = 3;
+            this.lb_CountRed.Text = "0";
+            // 
+            // lb_CountBlue
+            // 
+            this.lb_CountBlue.AutoSize = true;
+            this.lb_CountBlue.Location = new System.Drawing.Point(95, 50);
+            this.lb_CountBlue.Name = "lb_CountBlue";
+            this.lb_CountBlue.Size = new System.Drawing.Size(13, 13);
+            this.lb_CountBlue.TabIndex = 4;
+            this.lb_CountBlue.Text = "0";
+            // 
+            // panel_Score
+            // 
+            this.panel_Score.Controls.Add(this.lb_CountBlue);
+            this.panel_Score.Controls.Add(this.lb_CountRed);
+            this.panel_Score.Controls.Add(this.lb_Blue);
+            this.panel_Score.Controls.Add(this.lb_Red);
+            this.panel_Score.Location = new System.Drawing.Point(277, 23);
+            this.panel_Score.Name = "panel_Score";
+            this.panel_Score.Size = new System.Drawing.Size(156, 93);
+            this.panel_Score.TabIndex = 5;
+            this.panel_Score.Visible = false;
+            // 
+            // Puzzle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 379);
+            this.Controls.Add(this.panel_Score);
+            this.Controls.Add(this.panel_InputView);
+            this.Name = "Puzzle";
+            this.Text = "Puzzle";
+            this.panel_InputView.ResumeLayout(false);
+            this.panel_InputView.PerformLayout();
+            this.panel_Score.ResumeLayout(false);
+            this.panel_Score.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_InputView;
+        private System.Windows.Forms.Label lb_Welcome;
+        private System.Windows.Forms.Button btn_Play;
+        private System.Windows.Forms.TextBox tB_Input;
+        private System.Windows.Forms.Label lb_Input;
+        private System.Windows.Forms.Label lb_Red;
+        private System.Windows.Forms.Label lb_Blue;
+        private System.Windows.Forms.Label lb_CountRed;
+        private System.Windows.Forms.Label lb_CountBlue;
+        private System.Windows.Forms.Panel panel_Score;
+    }
+}
+
---
+++ W06 - Class Assignment - Puzzle/W06 - Class Assignment - Puzzle/Puzzle.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W06___Class_Assignment___Puzzle
+{
+    public partial class Puzzle : Form
+    {
+        //Evelin Alim Natadjaja
+        //NIM : 0706022310021
+
+        List<Button> listTiles = new List<Button>();
+        Button tile;
+        int inputUser = 0;
+        public Puzzle()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Play_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(tB_Input.Text) < 3)
+            {
+                MessageBox.Show("Please Input A Larger Size");
+            }
+            else
+            {
+                MessageBox.Show("Let's Play");
+                inputUser = Convert.ToInt32(tB_Input.Text);
+                panel_InputView.Visible = false;
+                panel_Score.Visible = true;
+            }
+        }
+
+        private void tB_Input_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void panel_InputView_VisibleChanged(object sender, EventArgs e)
+        {
+            int countLocationX = 0;
+            int countLocationY = 0;
+            for (int i = 0; i < inputUser; i++)
+            {
+                for (int j = 0; j < inputUser; j++)
+                {
+                    tile = new Button();
+                    tile.Size = new Size(50, 50);
+                    tile.Tag = "gray";
+                    tile.Location = new Point(countLocationX, countLocationY);
+                    countLocationX += 50;
+                    listTiles.Add(tile);
+                    this.Controls.Add(tile);
+                    tile.Click += new EventHandler(tile_Click);
+                }
+                countLocationX = 0;
+                countLocationY += 50;
+            }
+
+        }
+
+        private void tile_Click(object sender, EventArgs e)
+        {
+            Button tileClicked = (Button)sender;
+            int locationX = tileClicked.Location.X;
+            int locationY = tileClicked.Location.Y;
+            
+            for (int i = 0; i < listTiles.Count; i++)
+            {
+                if (listTiles[i].Location.X == locationX + 50 && listTiles[i].Location.Y == locationY && listTiles[i].Tag != "gray")
+                {
+                    if (listTiles[i].Tag == "red")
+                    {
+                        listTiles[i].Tag = "blue";
+                        listTiles[i].BackColor = Color.Blue;
+                    }
+                    else if (listTiles[i].Tag == "blue")
+                    {
+                        listTiles[i].Tag = "red";
+                        listTiles[i].BackColor = Color.Red;
+                    }
+                }
+                if (listTiles[i].Location.X == locationX && listTiles[i].Location.Y == locationY + 50 && listTiles[i].Tag != "gray")
+                {
+                    if (listTiles[i].Tag == "red")
+                    {
+                        listTiles[i].Tag = "blue";
+                        listTiles[i].BackColor = Color.Blue;
+                    }
+                    else if (listTiles[i].Tag == "blue")
+                    {
+                        listTiles[i].Tag = "red";
+                        listTiles[i].BackColor = Color.Red;
+                    }
+                }
+                if (listTiles[i].Location.X == locationX && listTiles[i].Location.Y == locationY - 50 && listTiles[i].Tag != "gray")
+                {
+                    if (listTiles[i].Tag == "red")
+                    {
+                        listTiles[i].Tag = "blue";
+                        listTiles[i].BackColor = Color.Blue;
+                    }
+                    else if (listTiles[i].Tag == "blue")
+                    {
+                        listTiles[i].Tag = "red";
+                        listTiles[i].BackColor = Color.Red;
+                    }
+                }
+                if (listTiles[i].Location.X == locationX - 50 && listTiles[i].Location.Y == locationY && listTiles[i].Tag != "gray")
+                {
+                    if (listTiles[i].Tag == "red")
+                    {
+                        listTiles[i].Tag = "blue";
+                        listTiles[i].BackColor = Color.Blue;
+                    }
+                    else if (listTiles[i].Tag == "blue")
+                    {
+                        listTiles[i].Tag = "red";
+                        listTiles[i].BackColor = Color.Red;
+                    }
+                }
+            }
+           
+
+            if (tileClicked.Tag == "gray")
+            {
+                tileClicked.BackColor = Color.Red;
+                tileClicked.Tag = "red";
+            }
+            else if (tileClicked.Tag == "red")
+            {
+                tileClicked.BackColor = Color.Blue;
+                tileClicked.Tag = "blue";
+            }
+            else if (tileClicked.Tag == "blue")
+            {
+                tileClicked.BackColor = Color.Red;
+                tileClicked.Tag = "red";
+            }
+
+            int countRed = 0;
+            int countBlue = 0;
+            foreach (Button tile in listTiles)
+            {
+                if (tile.Tag == "red")
+                {
+                    countRed++;
+                }
+                else if (tile.Tag == "blue")
+                {
+                    countBlue++;
+                }
+            }
+
+            lb_CountRed.Text = countRed.ToString();
+            lb_CountBlue.Text = countBlue.ToString();
+
+            if (countRed == inputUser*inputUser || countBlue == inputUser * inputUser)
+            {
+                MessageBox.Show("Congrats You Win!");
+            }
+        }
+    }
+}
