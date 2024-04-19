5b66742f85fb081422bfe89e0649508dd7b9dd66
evelinn11 <139670444+evelinn11@users.noreply.github.com>
2024-04-19 23:41:11 +0700

    Add files via upload

---
+++ W05 - Class Assignment - List Tim Sepak Bola/W05 - Class Assignment - List Tim Sepak Bola/List Tim Sepak Bola.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace W05___Class_Assignment___List_Tim_Sepak_Bola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dGV_Tim = new System.Windows.Forms.DataGridView();
+            this.lb_TimID = new System.Windows.Forms.Label();
+            this.lb_NamaTim = new System.Windows.Forms.Label();
+            this.lb_NamaStadium = new System.Windows.Forms.Label();
+            this.lb_Kapasitas = new System.Windows.Forms.Label();
+            this.lb_Kota = new System.Windows.Forms.Label();
+            this.lb_NamaManager = new System.Windows.Forms.Label();
+            this.tB_TimID = new System.Windows.Forms.TextBox();
+            this.tB_NamaTim = new System.Windows.Forms.TextBox();
+            this.tB_NamaStadium = new System.Windows.Forms.TextBox();
+            this.tB_Kapasitas = new System.Windows.Forms.TextBox();
+            this.tB_Kota = new System.Windows.Forms.TextBox();
+            this.tB_NamaManager = new System.Windows.Forms.TextBox();
+            this.btn_Input = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV_Tim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dGV_Tim
+            // 
+            this.dGV_Tim.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGV_Tim.Location = new System.Drawing.Point(9, 174);
+            this.dGV_Tim.Margin = new System.Windows.Forms.Padding(2);
+            this.dGV_Tim.Name = "dGV_Tim";
+            this.dGV_Tim.RowHeadersWidth = 82;
+            this.dGV_Tim.RowTemplate.Height = 33;
+            this.dGV_Tim.Size = new System.Drawing.Size(849, 316);
+            this.dGV_Tim.TabIndex = 0;
+            // 
+            // lb_TimID
+            // 
+            this.lb_TimID.AutoSize = true;
+            this.lb_TimID.Location = new System.Drawing.Point(27, 22);
+            this.lb_TimID.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_TimID.Name = "lb_TimID";
+            this.lb_TimID.Size = new System.Drawing.Size(38, 13);
+            this.lb_TimID.TabIndex = 1;
+            this.lb_TimID.Text = "Tim ID";
+            // 
+            // lb_NamaTim
+            // 
+            this.lb_NamaTim.AutoSize = true;
+            this.lb_NamaTim.Location = new System.Drawing.Point(27, 41);
+            this.lb_NamaTim.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_NamaTim.Name = "lb_NamaTim";
+            this.lb_NamaTim.Size = new System.Drawing.Size(55, 13);
+            this.lb_NamaTim.TabIndex = 2;
+            this.lb_NamaTim.Text = "Nama Tim";
+            // 
+            // lb_NamaStadium
+            // 
+            this.lb_NamaStadium.AutoSize = true;
+            this.lb_NamaStadium.Location = new System.Drawing.Point(27, 62);
+            this.lb_NamaStadium.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_NamaStadium.Name = "lb_NamaStadium";
+            this.lb_NamaStadium.Size = new System.Drawing.Size(76, 13);
+            this.lb_NamaStadium.TabIndex = 3;
+            this.lb_NamaStadium.Text = "Nama Stadium";
+            // 
+            // lb_Kapasitas
+            // 
+            this.lb_Kapasitas.AutoSize = true;
+            this.lb_Kapasitas.Location = new System.Drawing.Point(27, 81);
+            this.lb_Kapasitas.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_Kapasitas.Name = "lb_Kapasitas";
+            this.lb_Kapasitas.Size = new System.Drawing.Size(53, 13);
+            this.lb_Kapasitas.TabIndex = 4;
+            this.lb_Kapasitas.Text = "Kapasitas";
+            // 
+            // lb_Kota
+            // 
+            this.lb_Kota.AutoSize = true;
+            this.lb_Kota.Location = new System.Drawing.Point(27, 100);
+            this.lb_Kota.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_Kota.Name = "lb_Kota";
+            this.lb_Kota.Size = new System.Drawing.Size(29, 13);
+            this.lb_Kota.TabIndex = 5;
+            this.lb_Kota.Text = "Kota";
+            // 
+            // lb_NamaManager
+            // 
+            this.lb_NamaManager.AutoSize = true;
+            this.lb_NamaManager.Location = new System.Drawing.Point(27, 120);
+            this.lb_NamaManager.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_NamaManager.Name = "lb_NamaManager";
+            this.lb_NamaManager.Size = new System.Drawing.Size(80, 13);
+            this.lb_NamaManager.TabIndex = 6;
+            this.lb_NamaManager.Text = "Nama Manager";
+            // 
+            // tB_TimID
+            // 
+            this.tB_TimID.Enabled = false;
+            this.tB_TimID.Location = new System.Drawing.Point(109, 22);
+            this.tB_TimID.Margin = new System.Windows.Forms.Padding(2);
+            this.tB_TimID.Name = "tB_TimID";
+            this.tB_TimID.Size = new System.Drawing.Size(109, 20);
+            this.tB_TimID.TabIndex = 7;
+            // 
+            // tB_NamaTim
+            // 
+            this.tB_NamaTim.Location = new System.Drawing.Point(109, 41);
+            this.tB_NamaTim.Margin = new System.Windows.Forms.Padding(2);
+            this.tB_NamaTim.Name = "tB_NamaTim";
+            this.tB_NamaTim.Size = new System.Drawing.Size(109, 20);
+            this.tB_NamaTim.TabIndex = 8;
+            this.tB_NamaTim.TextChanged += new System.EventHandler(this.tB_NamaTim_TextChanged);
+            // 
+            // tB_NamaStadium
+            // 
+            this.tB_NamaStadium.Location = new System.Drawing.Point(109, 60);
+            this.tB_NamaStadium.Margin = new System.Windows.Forms.Padding(2);
+            this.tB_NamaStadium.Name = "tB_NamaStadium";
+            this.tB_NamaStadium.Size = new System.Drawing.Size(109, 20);
+            this.tB_NamaStadium.TabIndex = 9;
+            // 
+            // tB_Kapasitas
+            // 
+            this.tB_Kapasitas.Location = new System.Drawing.Point(109, 80);
+            this.tB_Kapasitas.Margin = new System.Windows.Forms.Padding(2);
+            this.tB_Kapasitas.Name = "tB_Kapasitas";
+            this.tB_Kapasitas.Size = new System.Drawing.Size(109, 20);
+            this.tB_Kapasitas.TabIndex = 10;
+            // 
+            // tB_Kota
+            // 
+            this.tB_Kota.Location = new System.Drawing.Point(109, 99);
+            this.tB_Kota.Margin = new System.Windows.Forms.Padding(2);
+            this.tB_Kota.Name = "tB_Kota";
+            this.tB_Kota.Size = new System.Drawing.Size(109, 20);
+            this.tB_Kota.TabIndex = 11;
+            // 
+            // tB_NamaManager
+            // 
+            this.tB_NamaManager.Location = new System.Drawing.Point(109, 118);
+            this.tB_NamaManager.Margin = new System.Windows.Forms.Padding(2);
+            this.tB_NamaManager.Name = "tB_NamaManager";
+            this.tB_NamaManager.Size = new System.Drawing.Size(109, 20);
+            this.tB_NamaManager.TabIndex = 12;
+            // 
+            // btn_Input
+            // 
+            this.btn_Input.Location = new System.Drawing.Point(71, 145);
+            this.btn_Input.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Input.Name = "btn_Input";
+            this.btn_Input.Size = new System.Drawing.Size(74, 27);
+            this.btn_Input.TabIndex = 13;
+            this.btn_Input.Text = "Input";
+            this.btn_Input.UseVisualStyleBackColor = true;
+            this.btn_Input.Click += new System.EventHandler(this.btn_Input_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 514);
+            this.Controls.Add(this.btn_Input);
+            this.Controls.Add(this.tB_NamaManager);
+            this.Controls.Add(this.tB_Kota);
+            this.Controls.Add(this.tB_Kapasitas);
+            this.Controls.Add(this.tB_NamaStadium);
+            this.Controls.Add(this.tB_NamaTim);
+            this.Controls.Add(this.tB_TimID);
+            this.Controls.Add(this.lb_NamaManager);
+            this.Controls.Add(this.lb_Kota);
+            this.Controls.Add(this.lb_Kapasitas);
+            this.Controls.Add(this.lb_NamaStadium);
+            this.Controls.Add(this.lb_NamaTim);
+            this.Controls.Add(this.lb_TimID);
+            this.Controls.Add(this.dGV_Tim);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "List Tim Sepak Bola";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dGV_Tim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dGV_Tim;
+        private System.Windows.Forms.Label lb_TimID;
+        private System.Windows.Forms.Label lb_NamaTim;
+        private System.Windows.Forms.Label lb_NamaStadium;
+        private System.Windows.Forms.Label lb_Kapasitas;
+        private System.Windows.Forms.Label lb_Kota;
+        private System.Windows.Forms.Label lb_NamaManager;
+        private System.Windows.Forms.TextBox tB_TimID;
+        private System.Windows.Forms.TextBox tB_NamaTim;
+        private System.Windows.Forms.TextBox tB_NamaStadium;
+        private System.Windows.Forms.TextBox tB_Kapasitas;
+        private System.Windows.Forms.TextBox tB_Kota;
+        private System.Windows.Forms.TextBox tB_NamaManager;
+        private System.Windows.Forms.Button btn_Input;
+    }
+}
+
---
+++ W05 - Class Assignment - List Tim Sepak Bola/W05 - Class Assignment - List Tim Sepak Bola/List Tim Sepak Bola.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W05___Class_Assignment___List_Tim_Sepak_Bola
+{
+    public partial class Form1 : Form
+    {
+        //Evelin Alim Natadjaja
+        //NIM : 0706022310021
+
+        DataTable dt;
+        string hurufPertama;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            dt.Columns.Add("ID Tim");
+            dt.Columns.Add("Nama Tim");
+            dt.Columns.Add("Nama Stadium");
+            dt.Columns.Add("Kapasitas");
+            dt.Columns.Add("Kota");
+            dt.Columns.Add("Nama Manager");
+            dGV_Tim.DataSource = dt;
+        }
+
+        private void btn_Input_Click(object sender, EventArgs e)
+        {
+            bool kembar = false;
+
+            foreach (DataGridViewRow row in dGV_Tim.Rows)
+            {
+                if (row.Cells[1].Value != null && row.Cells[1].Value.ToString() == tB_NamaTim.Text)
+                {
+                    kembar = true;
+                }
+                if (row.Cells[2].Value != null && row.Cells[2].Value.ToString() == tB_NamaStadium.Text)
+                {
+                    kembar = true;
+                }
+                if (row.Cells[5].Value != null && row.Cells[5].Value.ToString() == tB_NamaManager.Text)
+                {
+                    kembar = true;
+                }
+            }
+
+
+            if (kembar)
+            {
+                MessageBox.Show("Error", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                dt.Rows.Add(tB_TimID.Text, tB_NamaTim.Text, tB_NamaStadium.Text, tB_Kapasitas.Text, tB_Kota.Text, tB_NamaManager.Text);
+            }
+            tB_TimID.Text = string.Empty;
+            tB_NamaTim.Text = string.Empty;
+            tB_NamaStadium.Text = string.Empty;
+            tB_Kapasitas.Text = string.Empty;
+            tB_Kota.Text = string.Empty;
+            tB_NamaManager.Text = string.Empty;
+            hurufPertama = "";
+        }
+
+        private void tB_NamaTim_TextChanged(object sender, EventArgs e)
+        {
+            int count = 1;
+            if (tB_NamaTim.Text != string.Empty)
+            {
+                hurufPertama = tB_NamaTim.Text.Substring(0, 1);
+                foreach (DataGridViewRow row in dGV_Tim.Rows)
+                {
+                    if (row.Cells[1].Value != null && row.Cells[1].Value.ToString().StartsWith(hurufPertama))
+                    {
+                        count++;
+                    }
+                }
+                if (count < 10)
+                {
+                    tB_TimID.Text = hurufPertama.ToUpper() + "0" + count;
+                }
+                else
+                {
+                    tB_TimID.Text = hurufPertama.ToUpper() + count;
+                }
+            }
+        }
+    }
+}
